98930b25d9724f85cbd7265cf68db4235ae3ac42
Luiz <luizribeirocorreia09@gmail.com>
2026-06-24 14:12:20 -0300

    feat: Add allowAttackOnce
    a function that allow one player to attack one square
    because it'll be part of each player turn

--- src/controller/dom-manager.js
+++ src/controller/dom-manager.js
@@ -1,29 +1,49 @@
-import Player from "../classes/player-template.js";
 
 export default class DomManager {
-    #player;
-    #botPlayer;
 
-    constructor() {
-        this.#player = new Player();
-        this.#botPlayer = new Player();
-        this.pBoard = this.#player.board;
-        this.bBoard = this.#botPlayer.board;
+    constructor(playerBoard, botBoard) {
+        this.pBoard = playerBoard;
+        this.bBoard = botBoard;
     }
 
     refreashBoard(boardDiv) {
+        boardDiv.innerHTML = "";
         let boardObj = (boardDiv.dataset.player === "player") ? this.pBoard : this.bBoard;
-        const missedAttacks = boardObj.getMissedAttacks();
         for (let line = 0; line < 10; line++) {
-            for (let column = 0; column < 0; column++) {
+            for (let column = 0; column < 10; column++) {
                 const uiSqr = document.createElement("div");
+                const objSqr = boardObj.getSquare([line, column]);
+                if (line === 5 && column === 4) {
+                    console.log(objSqr);
+                }
                 uiSqr.classList.add("ui-sqr");
                 uiSqr.dataset.column = column;
                 uiSqr.dataset.line = line;
-                uiSqr.dataset.missedAttack = missedAttacks.includes(`${line},${column}`);
-                uiSqr.dataset.shipAttacked =
-                if (uiSqr.dataset.missedAttack) uiSqr.classList.add("missed");
+                uiSqr.dataset.isShip = (objSqr.shipPointer !== null) ? true : false;
+                uiSqr.dataset.missedAttack = (!objSqr.shipPointer && objSqr.isHit) ? true : false;
+                uiSqr.dataset.shipAttacked = (objSqr.shipPointer && objSqr.isHit) ? true : false;
+                if (uiSqr.dataset.missedAttack === "true") {
+                    uiSqr.textContent = "X";
+                    uiSqr.classList.add("miss-hit");
+                }
+                if (uiSqr.dataset.shipAttacked === "true") uiSqr.classList.add("hit");
+                if (uiSqr.dataset.isShip === "true") uiSqr.classList.add("ship");
+                boardDiv.appendChild(uiSqr);
             }
         }
+    }
+
+    attackAllowedOnce(boardDiv) {
+        boardDiv.addEventListener("click", (e) => {
+            const line = e.target.dataset.line;
+            const column = e.target.dataset.column;
+            this.pBoard.receiveAttack([Number(line), Number(column)]);
+            console.log(line, column);
+            this.refreashBoard(boardDiv);
+        }, {once: true})
+    }
+
+    allowInsertShipUi(){
+
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./template.css";
+import DomManager from "./controller/dom-manager.js";
+import Player from "./classes/player-template.js";
+
+const pBoardUi = document.querySelector(".board-1");
+const bBoardUi = document.querySelector(".board-2");
+const p = new Player();
+const bp = new Player();
+const pBoardObj = p.board;
+const bBoardObj = bp.board;
+
+pBoardObj.placeShip(5, [5,4], "v");
+
+const gameUi = new DomManager(pBoardObj, bBoardObj);
+
+gameUi.refreashBoard(pBoardUi);
+gameUi.refreashBoard(bBoardUi);
+gameUi.attackAllowedOnce(pBoardUi);
+
+// console.log(document.querySelector(".ship"));
