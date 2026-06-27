6be03a4518c715800124483e7818b95c401f9a53
Luiz <luizribeirocorreia09@gmail.com>
2026-06-27 16:10:02 -0300

    feat: Add tests for bot class

---
+++ tests/bot.spec.js
@@ -0,0 +1,33 @@
+import Bot from "../src/classes/bot-template.js";
+import Board from "../src/classes/game-board-template.js";
+
+let bot = new Bot();
+let board = new Board();
+
+describe('random attack', () => {
+    beforeEach(() => {
+        bot = new Bot();
+        board = bot.board;
+    });
+
+    test('attack five times itself', () => {
+        bot.randomAttack(board);
+        bot.randomAttack(board);
+        bot.randomAttack(board);
+        bot.randomAttack(board);
+        bot.randomAttack(board);
+        expect(board.missedHits.length).toBe(5);
+    });
+})
+
+describe('random deploy', () => {
+    beforeEach(() => {
+        bot = new Bot();
+        board = bot.board;
+    });
+
+    test('deploy its five ships', () => {
+        bot.randomDeploy();
+        expect(board.ships.length).toBe(5);
+    });
+});
